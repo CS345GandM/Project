cefdba8b24c78fdc44322608b71a11aa3e1c0e8b
CS345GandM <montagh@wwu.edu>
2017-05-08 12:05:35 -0700

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -1,34 +1,65 @@
 //Melissa Gonzalez and Hannah Montague
 
 import java.util.*;
+import java.io.*;
 
-public class GameBoard{
-   //attributes
-   public String[] board;
-   public String[] player;
-   private int day;
-   private int totalScenes;
-   public int remainingScenes;
+public class Board{
+  private static int numPlayers = 0;
+  private static int numDays = 0;
+  private static int numCards = 0;
+  private Scanner setScanner = readFile("Set.txt");
+  private ArrayList<Set> sets = new ArrayList<>();
 
-   public static void main(String[] args){
+  // Constructor takes one input : the number of players
+  Board(int numPlayers) {
+      this.numPlayers = numPlayers;
+  }
 
-   }
 
-   //changes based on number of players
-   public void setDays(int x){
-   }
+  private Scanner readFile(String fileName) {
+      Scanner scan = null;
+      try {
+          scan = new Scanner(new File(fileName));
+      } catch (FileNotFoundException e) {
+          System.out.println("Error: unable to open file: " + fileName);
+          System.exit(1);
+      }
+      return scan;
+  }
 
-   //necessary?
-   public int getDay(){
-      return day;
-   }
+//according to game rules
+  private void setDays() {
+      if (numPlayers == 2 || numPlayers == 3) {
+          numDays = 3;
+      } else {
+          numDays = 4;
+      }
+  }
 
-   public void newDay(){
-   }
+  public int changeTurn(int turn) {
+    if (turn < 1) {
+        turn ++;
+    }
+    else {
+        turn =0;
+    }
+    return turn;
+}
 
-   public void playerTurns(){
-   }
 
-   public void finalScore(){
-   }
+  public int finalScore(Player player) {
+    int score = calcScore(player);
+    System.out.println("--------------------------------------");
+    System.out.println("Name:             " + player.getName());
+    System.out.println("Rank:             " + player.getRank());
+    System.out.println("Credit:           " + player.getCredit());
+    System.out.println("Money:            " + player.getMoney());
+    System.out.println("Total Score:      " + score);
+    System.out.println("--------------------------------------");
+    return score;
+  }
+
+  public int calcScore(Player player) {
+    return player.getMoney() + player.getCredit() + (player.getRank() * 5);
+  }
 }
