9f009ba290e7c2abc59a322c14d0626fb0d4fb61
Hannah Montague <montagh@LabImage2016-17>
2017-05-09 13:04:54 -0700

    Progress on read in. Hope this works

--- Rooms.java
+++ Rooms.java
@@ -1,50 +1,47 @@
 //Melissa Gonzalez and Hannah Montague
-/*Name
-number of connections
-connection names
-.
-.
-*/
+
 
 import java.util.*;
 import java.lang.*;
 
-///NOT SETS!!!!!!!!
 public class Rooms{
-   private Set one;
-   private Set two;
-   private Set three;
-   private Set four;
-   private Set name;
+   private String one;
+   private String two;
+   private String three;
+   private String four;
+   private String name;
 
    public Rooms(){
    }
 
-   private Rooms(String up, String down, String left, String right, String name){
-      this.one = null;
-      this.two = null;
-      this.three = null;
-      this.four = null;
+   private Rooms(String one, String two, String three, String four, String name){
+      this.one = one;
+      this.two = two;
+      this.three = three;
+      this.four = four;
       this.name = name;
    }
 
-   public void successfulAct(){
+  /* public void successfulAct(){
      name.removeCounters();
    }
    public int remainingShots(){
      int x = name.getCounts();
      return x;
+   }*/
+   public String peekOne(){
+     return one
    }
-   public Set peekOne(){
-     return one.getName();
+   public String peekTwo(){
+     return two;
    }
-   public Set peekTwo(){
-     return two.getName();
+   public String peekThree(){
+     return three;
    }
-   public Set peekThree(){
-     return three.getName();
+   public String peekFour(){
+     return four;
    }
-   public Set peekFour(){
-     return four.getName();
+   public String peekName(){
+     return name;
    }
 }
