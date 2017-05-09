73a06719f4b27668791ca606e077716e83845464
Melissa Gonzalez <gonzalm7@LabImage2016-17>
2017-05-09 13:07:17 -0700

    Changed Cards

--- Cards.java
+++ Cards.java
@@ -1,25 +1,40 @@
 //Melissa Gonzalez and Hannah Montague
-/*Name
-Budget NumRoles roleValue . . */
+/*Name Budget NumRoles roleValue . . */
 import java.util.*;
-import java.lang.*;
 public class Cards{
 
    private String name;
    private int budget;
-   private int[] roles;
+   private String line;
+   private ArrayList<Role> roles;
 
-   private Cards(int[] roles, int budget, String name){
+   private Cards(ArrayList<Role> roles, int budget, String name, String line){
       this.name = name;
       this.budget = budget;
       this.roles = roles;
+      this.line = line;
    }
    private void makeCard(){
       //read in text file
    }
 
+
+ 	public Role getRoleName(String role) {
+ 		Role name = null;
+
+ 		for(Role current : role) {
+ 			if(current.getRoleTitle().equals(role))
+ 				name = current;
+ 		}
+
+ 		return name;
+ 	}
    public String getName(){
       return name;
+   }
+
+   public String getQuote(){
+     return line;
    }
 
    public int getBudget(){
@@ -29,5 +44,7 @@ public class Cards{
    public void turnCard(){
       return;
    }
+
+
 
 }
