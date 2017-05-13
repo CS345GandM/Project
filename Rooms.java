79935b6023b32c1071ec6a6e7cdeceedb548a429
MelissaGnz <gonzalm7@wwu.edu>
2017-05-13 13:02:02 -0700

    Added Comments to Rooms

--- Rooms.java
+++ Rooms.java
@@ -1,16 +1,17 @@
 //Melissa Gonzalez and Hannah Montague
-
-
+//Room Class - The gameboard contains different Rooms
 import java.util.*;
 import java.lang.*;
 
 public class Rooms{
-   private String one;
-   private String two;
-   private String three;
-   private String four;
-   private String name;
+  //there are a max of 4 rooms per board section
+   private String one;   //name of room one
+   private String two;   //name of room two
+   private String three; //name of room three
+   private String four;  //name of room four
+   private String name;  //name of room
 
+   //Constuctor for Room
    public Rooms(String one, String two, String three, String four, String name){
       this.one = one;
       this.two = two;
@@ -25,6 +26,10 @@ public class Rooms{
      int x = name.getCounts();
      return x;
    }*/
+
+   //Want to take a look at rooms to decide whether they are adj
+   //Can only move to adj room
+
    public String peekOne(){
      return one;
    }
