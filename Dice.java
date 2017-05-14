4c85178a8469e3a5c64e3f159d6dfd78ceaa72f8
MelissaGnz <gonzalm7@wwu.edu>
2017-05-13 19:34:59 -0700

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,5 +1,5 @@
 //Melissa Gonzalez and Hannah Montague
-//Dice Class - Generates a random number 1-6
+//Dice Class - Simulates a die by generating a random number 1-6
 import java.util.*;
 import java.lang.*;
 import java.util.Random;
@@ -13,9 +13,9 @@ public class Dice{
       this.rollOutcome = 0; 
    }
 
-   //Method: getValue()
+   //Method:  getValue()
    //Purpose: generate a random number(1-6)
-   //Return: int
+   //Output:  int
    public int getValue(){
      Random random = new Random();
      rollOutcome = random.nextInt(6) + 1;
