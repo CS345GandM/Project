6a4362c69c8b6ca3761a476a80133e870c1f97e7
MelissaGnz <gonzalm7@wwu.edu>
2017-05-13 18:59:19 -0700

    Added Comments to Cards

--- Cards.java
+++ Cards.java
@@ -5,12 +5,13 @@ import java.lang.*;
 
 public class Cards{
 
-   private String name;
-   private String description;
-   private int sceneNum;
-   private int budget;
+   private String name;         //Name of Card    
+   private String description;  //Card Description
+   private int    sceneNum;     //Card contains a scene number
+   private int    budget;       //Card has its own budget
    private ArrayList<Role> onCardRoles = new ArrayList<Role>();
-
+   
+   //Card constructor 
    public Cards(String n, int b, int sN, ArrayList<Role> r, String d){
       this.name = n;
       this.description = d;
@@ -18,27 +19,39 @@ public class Cards{
       this.sceneNum = sN;
       this.onCardRoles = r;
    }
-
+ 
    private void makeCard(){
       //read in text file
    }
+   
+   //Getter:  getNumRoles
+   //Purpose: Get the number of roles on a card
    public int getNumRoles(){
      int len = onCardRoles.size();
      return len;
    }
-
+   
+   //Getter:  getName
+   //Purpose: get the name of a card
    public String getName(){
       return name;
    }
-
+   //Getter:  getQuote
+   //Purpose: get the description of a card
    public String getQuote(){
      return description;
    }
-
+   
+   //Getter:  getBudget
+   //Purpose: get the card's budget
    public int getBudget(){
       return budget;
    }
-
+   
+   //Method:  isARole()
+   //Purpose: check if the player's role is contained in the onCardRoles array
+   //Input:   String - name of a role
+   //Output:  boolean
    public boolean isARole(String roleName){
      for(Role r : onCardRoles){
        String currRole = r.getRoleTitle();
@@ -48,7 +61,11 @@ public class Cards{
      }
      return false;
    }
-
+   
+   //Method:  getRoleRanks()
+   //Purpose: get the values of all the roles
+   //Input:   none
+   //Output:  an array of integers sorted
    public int[] getRoleRanks(){
      int[] ranks = new int[onCardRoles.size()];
      int track = 0;
@@ -59,7 +76,4 @@ public class Cards{
      Arrays.sort(ranks);
      return ranks;
    }
-
-
-
 }
